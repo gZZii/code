891df6d1d19311bc4b58169e88cb073f3ae06b22
gZii <guillaume.adelet@gmail.com>
2024-05-18 12:51:25 +0200

    modification de script.js après deploiement

--- selectionCDA-Simplon/public/script.js
+++ selectionCDA-Simplon/public/script.js
@@ -1,7 +1,7 @@
 const blagueButton = document.getElementById('blague-button');
 
 blagueButton.addEventListener('click', () => {
-  fetch('http://localhost:3000/blagues/random')
+  fetch('https://selectioncda.onrender.com/blagues/random')
     .then((response) => response.json())
     .then((data) => {
       const blague = document.getElementById('blague');
